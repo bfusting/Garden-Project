a9675fd751f7abbeed9b5c2880bcf00396ad4e23
Malachi Parks <MalachiParks1999@gmail.com>
2020-04-19 20:56:44 -0400

    Malachi Parks added getters and methods to the Model.java and started to edit the Controller.java with a constructor and attributes

--- Controller.java
+++ Controller.java
@@ -1,4 +1,30 @@
+/*
+*  Authors: Team 11-3: Bradley Fusting, Takiyah Price, Kelsey McRae, Malachi Parks
+*  Class Section: 011
+*  Lab Section: 031L
+*  TA: Vineeth Gutta
+*  Due: May 18th, 2020 (5/18/20)
+*
+*  This file contains the contents for the project for CISC275. The project for the 
+*  class is to make gardening software for the township of Arden, DE to help
+*  promote forest edge preservation. 
+*  
+*/
 
+/**
+ * 
+ * @author Malachi Parks
+ *
+ */
 public class Controller {
-
+	private Model model;
+	private View view;
+	
+	/**
+	 * 
+	 */
+	public Controller() {
+		model = new Model();
+		view = new View();
+	}
 }
