f82c0c9518082c54910963b93eb50263c708a9a2
Malachi Parks <MalachiParks1999@gmail.com>
2020-04-19 23:45:11 -0400

    Malachi Parks: Mostly done with method stubs for Controller, need to still add getters which return eventHandlers for each and create a blank test file for Controller

--- Controller.java
+++ Controller.java
@@ -1,3 +1,4 @@
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 /*
@@ -39,8 +40,109 @@ public class Controller {
 		//launch();
 	}//main
 	
+	/**
+	 * 
+	 * @param stage
+	 */
 	public static void start(Stage stage) {
-		
-	}
+		System.out.println("Do Something");
+	}//start
 	
+	/**
+	 * 
+	 * @param event
+	 */
+	public void createNewGarden(MouseEvent event) {
+		System.out.println("NEW GARDEN HAHAHAHAHAH");
+	}//createNewGarden
+	
+	
+	/**
+	 * 
+	 * @param event
+	 */
+	public void loadGarden(MouseEvent event) {
+		System.out.println("Load Garden Here");
+	}//loadGarden
+	
+	
+	/**
+	 * 
+	 * @param event
+	 */
+	public void exit(MouseEvent event) {
+		System.out.println("Exit by saving the closing");
+	}//exit
+	
+	/**
+	 * 
+	 * @param event
+	 */
+	public void instructionsShow(MouseEvent event) {
+		System.out.println("Show Instructions");
+	}//instructionsShow
+	
+	/**
+	 * 
+	 * @param event
+	 */
+	public void backBTN(MouseEvent event) {
+		System.out.println("Go Back");
+	}//backBTN
+	
+	/**
+	 * 
+	 * @param event
+	 */
+	public void chooseDesign(MouseEvent event) {
+		System.out.println("Choose Templatttteeeeee");
+	}//choseDesign
+	
+	/**
+	 * 
+	 * @param event
+	 */
+	public void comboCheckBox(MouseEvent event) {
+		System.out.println("click me!");
+	}//comboCheckBox
+	
+	/**
+	 * 
+	 * @param event
+	 */
+	public void recommendationsBTN(MouseEvent event) {
+		System.out.println("WE RECOMMEND COVID19");
+	}//recommendationsBTN
+	
+	/**
+	 * 
+	 * @param event
+	 */
+	public void changeSeasonsBTN(MouseEvent event) {
+		System.out.println("Now is FAAAAAAAAAAAALLLLLLLLLLLLLLLLLL");
+	}//changeSeasonsBTN
+	
+	/**
+	 * 
+	 * @param event
+	 */
+	public void mainMenuBTN(MouseEvent event) {
+		System.out.println("Take Me Home");
+	}//mainMenuBTN
+	
+	/**
+	 * 
+	 * @param event
+	 */
+	public void changeTab(MouseEvent event) {
+		System.out.println("change tabbbbb");
+	}//changeTab
+	
+	/**
+	 * 
+	 * @param event
+	 */
+	public void startDrag(MouseEvent event) {
+		System.out.println("Starting to drag my soul out of my bodddyyyyyy");
+	}//startDrag
 }//Controller
