0b26cac14339ec54713fdae3ca71e9efd991caec
Malachi Parks <MalachiParks1999@gmail.com>
2020-04-21 14:47:58 -0400

    Malachi Parks: Editing the Controller.java and their respective tests to test for event handlers

--- ModelTest.java
+++ ModelTest.java
@@ -2,12 +2,19 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+/*
+*  Authors: Team 11-3: Bradley Fusting, Takiyah Price, Kelsey McRae, Malachi Parks
+*  Class Section: 011
+*  Lab Section: 031L
+*  TA: Vineeth Gutta
+*  Due: May 18th, 2020 (5/18/20)
+*
+*  This file contains the contents for the project for CISC275. The project for the 
+*  class is to make gardening software for the township of Arden, DE to help
+*  promote forest edge preservation. 
+*  
+*/
 
-/**
- * 
- * @author Malachi Parks
- *
- */
 class ModelTest {
 
 	@Test
@@ -26,22 +33,67 @@ class ModelTest {
 	}
 
 	@Test
-	void testFillSeclectionArr() {
+	void testUpdateSeclectionArr() {
 		fail("Not yet implemented");
 	}
 
 	@Test
-	void testFillShrubArr() {
+	void testUpdateShrubArr() {
 		fail("Not yet implemented");
 	}
 
 	@Test
-	void testFillTreeArr() {
+	void testUpdateTreeArr() {
 		fail("Not yet implemented");
 	}
 
 	@Test
-	void testFillUnderGrowthArr() {
+	void testUpdateUnderGrowthArr() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetUserPlot() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetSelectionArr() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetShrubArr() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetTreeArr() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetUnderGrowth() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetSelectionArr() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetShrubArr() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetTreeArr() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetUnderGrowthArr() {
 		fail("Not yet implemented");
 	}
 
