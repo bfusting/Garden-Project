3d1608bcbce361a7e2491e63445ccf120f1e3925
Bradley <bfusting@udel.edu>
2020-04-21 20:38:43 -0400

    Adding plant.java

---
+++ Plant.java
@@ -0,0 +1,27 @@
+import java.utils.*;
+
+public class Plant extends AddOn {
+
+	String color;
+	int waterNeed;
+	int sunLightNeed;
+	double plantHeight;
+	int locationPref;
+	Date bloomTime;
+	boolean provideShade;
+	
+	String[] seasonImages;
+	String[] ageImages;
+	
+	String plantType;
+	
+	ArrayList<String> animalsFed = new ArrayList();
+	ArrayList<String> enemyPlant = new ArrayList();
+	
+	/**
+	 * 
+	 */
+	public void Plant() {
+		
+	}
+}
