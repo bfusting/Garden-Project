4d868404822e092ef739efab983c55f00af1b3ab
Bradley <bfusting@udel.edu>
2020-04-24 14:13:40 -0400

    Bradley: updated GardenTile, GardenPlot, methods and tests

--- GardenPlotTest.java
+++ GardenPlotTest.java
@@ -29,24 +29,35 @@ public class GardenPlotTest {
 	 * @param x
 	 * @param y
 	 */
-	void testIsSurroundingEmpty(int x, int y) {
+	void testIsSurroundingEmpty() {
 		GardenPlot gP = new GardenPlot();
 		GardenTile gT = new GardenTile();
 		GardenTile[][] gA = {{gT}};
 		gP.setLayout(gA);
 		
-		assertTrue(gP.isSurroundingEmpty(x, y));
+		assertTrue(gP.isSurroundingEmpty(0,0));
 		GardenTile g1 = new GardenTile();
 		GardenTile g2 = new GardenTile();
 		GardenTile g3 = new GardenTile();
-		GardenTile[][] gB = {{g1, g2, g3}};
+		GardenTile g4 = new GardenTile();
+		GardenTile g5 = new GardenTile();
+		GardenTile g6 = new GardenTile();
+		GardenTile g7 = new GardenTile();
+		GardenTile g8 = new GardenTile();
+		GardenTile g9 = new GardenTile();
+		
+		g5.setxLoc(1);
+		g5.setyLoc(1);
+		GardenTile[][] gB = {{g1, g2, g3},
+							 {g4, g5, g6},
+							 {g7, g8, g9}};
 		
 		AddOn a = new AddOn();
 		g1.add(a);
 		g2.add(a);
 		g3.add(a);
 		
-		assertFalse(gP.isSurroundingEmpty(x, y));
+		assertFalse(gP.isSurroundingEmpty(1, 1));
 		
 	}
 	
@@ -66,5 +77,22 @@ public class GardenPlotTest {
 				assertTrue(t.getAddOn() != null);
 			}
 		}
+	}
+	
+	@Test
+	/**
+	 * Creates a GardenPlot and four GardenTiles to test if getSurroundingInfo() returns an array list of surrounding tiles AddOns
+	 */
+	void testGetSurroundingInfo() {
+		GardenPlot gP = new GardenPlot();
+		GardenTile g1 = new GardenTile();
+		g1.setxLoc(1);
+		g1.setyLoc(1);
+		GardenTile g2 = new GardenTile();
+		GardenTile g3 = new GardenTile();
+		GardenTile[][] gB = {{g1, g2, g3}};
+		gP.setLayout(gB);
+		AddOn[] aA = {g1.getAddOn(), g3.getAddOn()};
+		assertTrue(gP.getSurroundingInfo(1,1) == aA);
 	}
 }
