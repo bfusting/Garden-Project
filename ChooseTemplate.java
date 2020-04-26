b5364a29c203b851242d3dcc2b9bcedc9bcf97c3
Takiyah Price <tjprice99@me.com>
2020-04-26 19:37:18 -0400

    binding square choose template button to eventhandlers modified ChooseTemplate Controller View

--- ChooseTemplate.java
+++ ChooseTemplate.java
@@ -1,8 +1,18 @@
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.TilePane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 /*
@@ -18,7 +28,7 @@ import javafx.stage.Stage;
 *  
 */
 
-//Last edited 4-26-20 5:42PM
+//Last edited 4-26-20 6:46PM
 
 /**
  * 
@@ -32,10 +42,10 @@ import javafx.stage.Stage;
 
 public class ChooseTemplate extends Screen {
 	private Scene chooseTemplateScene;
-	private ImageView squareTemplate;
-	private ImageView circleTemplate;
-	private ImageView triangleTemplate;
-	private ImageView designYourOwn;
+	//private ImageView squareTemplate;
+	//private ImageView circleTemplate;
+	//private ImageView triangleTemplate;
+	//private ImageView designYourOwn;
 	private Button chooseSquare;
 	private Button chooseCircle;
 	private Button chooseTriangle;
@@ -48,12 +58,57 @@ public class ChooseTemplate extends Screen {
 	 * 
 	 */
 	public ChooseTemplate(Controller c) {
-		Label label = new Label("choose a template");
-		Button button = new Button("Exit");
-		button.setMinSize(200,60);
+		//Label label = new Label("choose a template");
+		//Button button = new Button("Exit");
+		//button.setMinSize(200,60);
 		
-		button.setOnMouseClicked(c.getExit());
-		chooseTemplateScene = new Scene(button,1200,800);
+		//need to get images
+		AnchorPane chooseTemplateAP = new AnchorPane();
+		chooseTemplateAP.setBackground(new Background(new BackgroundFill(Color.DARKCYAN,CornerRadii.EMPTY,Insets.EMPTY)));
+		
+		ImageView squareTemplate = new ImageView(new Image("/img/Square.png"));
+		ImageView circleTemplate = new ImageView(new Image("/img/Circle.png"));
+		ImageView triangleTemplate = new ImageView(new Image("/img/Triangle.png"));
+		ImageView designYourOwn = new ImageView(new Image("/img/DesignYourOwn.png"));
+		chooseSquare = new Button("Choose Square Garden",squareTemplate);
+		chooseSquare.setMaxSize(200, 60);
+		chooseSquare.setOnMouseClicked(c.getChooseDesign());
+		
+		//HBox stHBox = new HBox();
+		//HBox tdHBox = new HBox();
+		TilePane tPane = new TilePane();
+		
+		
+		chooseCircle = new Button("Choose Circular Garden",circleTemplate);
+		
+		
+		chooseCircle.setMinSize(200, 60);
+		chooseTriangle = new Button("Choose Triangular Garden",triangleTemplate);
+		chooseTriangle.setMinSize(200, 60);
+		chooseDesign = new Button("Design Your Own",designYourOwn);
+		chooseDesign.setMinSize(200,60);
+		
+		//stHBox.getChildren().addAll(chooseSquare,chooseTriangle);
+		//tdHBox.getChildren().addAll(chooseTriangle,chooseDesign);
+		
+		
+		//tPane.getChildren().addAll(stHBox,tdHBox);
+		
+		tPane.getChildren().addAll(chooseSquare, chooseCircle,chooseTriangle,chooseDesign);
+		tPane.setHgap(100);
+		tPane.setVgap(100);
+		tPane.setPrefColumns(2);
+		tPane.setAlignment(Pos.CENTER);
+		
+		
+		
+		AnchorPane.setLeftAnchor(tPane, 40.0);
+		AnchorPane.setRightAnchor(tPane, 40.0);
+		AnchorPane.setTopAnchor(tPane, 80.0);
+		chooseTemplateAP.getChildren().add(tPane);
+		
+		//button.setOnMouseClicked(c.getExit());
+		chooseTemplateScene = new Scene(chooseTemplateAP,1200,800);
 		
 		
 		
