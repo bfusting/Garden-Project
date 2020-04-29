aebd1932f350cbdc7f8953742d3be4c9a59341f7
Takiyah Price <tjprice99@me.com>
2020-04-29 19:01:57 -0400

    getting changes from master Controller DesignGarden .gitignore and new image soil.jpg

--- Controller.java
+++ Controller.java
@@ -113,10 +113,7 @@ public class Controller{
 		System.out.println("Load Garden Here");
 		File file = view.showLoadGardenScreen();
 		if (file!=null) {
-		//presumably update Model here with file contents and tell all the information to view
 		view.showDesignGardenScreen();
-		} else {
-			view.goToLastScreen();
 		}
 	}//loadGarden
 	
@@ -610,12 +607,6 @@ public class Controller{
 	public void saveBTN(MouseEvent event) {
 		System.out.println("SAVE OFTEN");
 		File file = view.showSaveGardenScreen();
-		if (file!=null) {
-			//write model and view stuff to the file
-		} else {
-			view.goToLastScreen();
-		}
-		
 	}//saveBTN
 	
 	/**
