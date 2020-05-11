c2969c88e1bd299160d995174ea2c0ebb06ca0fa
Malachi Parks <MalachiParks1999@gmail.com>
2020-05-10 23:40:46 -0400

    Malachi Parks: Removed uncessary attributes and replaced them with new buttons for each season

--- SeasonView.java
+++ SeasonView.java
@@ -2,11 +2,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.text.Text;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 /*
@@ -37,12 +33,26 @@ public class SeasonView extends Screen{
 	// full screen season view?
 	private final static int HEIGHT = 600;
 	private final static int WIDTH = 800;
-	private Label yourGarden;
-	private Label currSeason;
-	private BorderPane currView;
-	private Scene seasonView;
-	private Button backBTN;
-	private Button nextBTN;
+
+	//used as the main stage which holds everything
+	private AnchorPane root;
+	
+	// displays current gridpane from designGarden
+	private GridPane seasonGP;
+	
+	// used to change between various seasons by setting various scenes
+	private Scene springView;
+	private Scene summerView;
+	private Scene fallView;
+	private Scene winterView;
+	
+	// Buttons used to swap between seasons
+	private Button springBTN;
+	private Button summerBTN;
+	private Button fallBTN;
+	private Button WinterBTN;
+	
+	// used to display stage
 	private Controller c;
 	private Stage stage;
 	
@@ -51,59 +61,29 @@ public class SeasonView extends Screen{
 	 * to be created by any other class
 	 */
 	public SeasonView(Controller controller) {
-		yourGarden = new Label("Your Garden");
-		currSeason = new Label("Spring");
-		currView = new BorderPane();
-		seasonView = null;
 		c = controller;
-	}
-	
-	/**
-	 * Returns type Label which displays the current season name in the SeasonsView
-	 * pane.
-	 * <p>
-	 * Getter for CurrSeason attribute
-	 * 
-	 * @return Label which represents the scenes current seasons
-	 * @see currSeason
-	 */
-	public Label getCurrSeason() {
-		return currSeason;
-	}
-
-	/**
-	 * Returns type Scene which displays the current season of the SeasonsView
-	 * pane.
-	 * <p>
-	 * Getter for seasonView attribute
-	 * 
-	 * @return scene which represents the current season on the screen
-	 * @see seasonView
-	 * 
-	public Scene getSeasonView() {
-		return seasonView;
 	}
 	
 	/**
 	 * Returns the back button to bind an event listener to
 	 * <p>
-	 * Getter for backBTN
+	 * Getter for springBTN
 	 * 
 	 * @return back button on the screen
 	 */
-	public Button getbackBTN() {
-		return backBTN;
+	public Button getSpringBTN() {
+		return springBTN;
 	}
 	
 	/**
 	 * Returns the next button to bind an event listener to
 	 * <p>
-	 * Getter for nextBTN
+	 * Getter for summerBTN
 	 * 
-	 * @return next button on the screen
+	 * @return summer button on the screen
 	 */
-	public Button getNextBTN() {
-		return nextBTN;
+	public Button getSummerBTN() {
+		return summerBTN;
 	}
 	
 	/**
@@ -116,27 +96,11 @@ public class SeasonView extends Screen{
 	public void ShowSeasonView(){
 		//Creating new instances of images
 		stage = new Stage();
-		BorderPane bPane = new BorderPane();
-		Text texInFlow = new Text("Here will be the season presentation of the Garden"
-				+ " in various seasons. Buttons will change the title of the scene" );
-		FlowPane fPane = new FlowPane();
-		HBox hPane = new HBox();
-		backBTN = new Button("Back a Season");
-		nextBTN = new Button("Next Season");
-		Pane spacer = new Pane();
-		spacer.setMinSize(500, 1);
 		
-		// Stacking items within items buttons -> HBox --> Bottom of bpane
-		hPane.getChildren().add(spacer);
-		hPane.getChildren().add(backBTN);
-		hPane.getChildren().add(nextBTN);
-		fPane.getChildren().add(texInFlow);
-		bPane.setBottom(hPane);
-		bPane.setCenter(fPane);
-		bPane.setTop(currSeason);
+		root = new AnchorPane();
 		
 		// Setting the Scene with bPane
-		Scene scene = new Scene(bPane,HEIGHT,WIDTH);
+		Scene scene = new Scene(root,HEIGHT,WIDTH);
 		stage.setTitle("SeasonView");
 		stage.setScene(scene);
 		stage.show();
